9d650c4ff3b7145b57e901ed129b2ec8ff534ec0
Vasia Leleka <vasialeleka@ukr.net>
2019-05-21 01:53:23 -0700

    if else for buttons

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var  numberFromScreen : Double = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,8 +23,23 @@ class ViewController: UIViewController {
 
     @IBAction func digits(_ sender: UIButton) {
        result.text = result.text! + String(sender.tag)
+        numberFromScreen = Double(result.text!)!
+        
     
     }
     
+    @IBAction func buttons(_ sender: UIButton) {
+        if result.text != ""
+        { if sender.tag == 11 {
+            //Ділення
+            }else if sender.tag == 12 {
+                //Множення
+            }else if sender.tag == 13 {
+                //Віднімання
+            }else if sender.tag == 14 {
+                //Додавання
+            }
+         }
+        }
 }
 
