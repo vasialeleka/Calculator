9d6678349bc566d2904511b0cc3f106036a35a37
Vasia Leleka <vasialeleka@ukr.net>
2019-05-21 01:42:35 -0700

    number click

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -15,11 +15,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var result: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func digits(_ sender: UIButton) {
+       result.text = result.text! + String(sender.tag)
+    
+    }
+    
 }
 
