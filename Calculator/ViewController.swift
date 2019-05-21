cb804d491d1fca144fd12671fb1eac7e5cd8095f
Vasia Leleka <vasialeleka@ukr.net>
2019-05-21 02:33:23 -0700

    finish app

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     var  numberFromScreen : Double = 0
+    var firstNumber : Double = 0
+    var mathSign : Bool = false
+    var operation : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,24 +25,45 @@ class ViewController: UIViewController {
     }
 
     @IBAction func digits(_ sender: UIButton) {
-       result.text = result.text! + String(sender.tag)
-        numberFromScreen = Double(result.text!)!
-        
-    
+        if mathSign == true {
+            result.text = String(sender.tag)
+            mathSign = false
+        } else {
+            result.text = result.text! + String(sender.tag)
+                  }
+       
+         numberFromScreen = Double(result.text!)!
     }
     
     @IBAction func buttons(_ sender: UIButton) {
-        if result.text != ""
-        { if sender.tag == 11 {
-            //Ділення
+        if result.text != "" && sender.tag != 15 && sender.tag != 10
+            
+        { firstNumber = Double(result.text!)!
+            if sender.tag == 11 {
+            result.text = "/"
             }else if sender.tag == 12 {
-                //Множення
+                result.text = "x"
             }else if sender.tag == 13 {
-                //Віднімання
+            result.text = "-"
             }else if sender.tag == 14 {
-                //Додавання
+                result.text = "+"
             }
-         }
+            operation = sender.tag
+            mathSign = true
+        } else if sender.tag == 15 {
+            //equels
+            if operation == 11 {result.text = String(firstNumber / numberFromScreen)}
+            else if operation == 12 {result.text = String(firstNumber * numberFromScreen)}
+            else if operation == 13 {result.text = String(firstNumber - numberFromScreen)}
+            else if operation == 14 {result.text = String(firstNumber + numberFromScreen)}
+        
+        } else if sender.tag == 10{
+            result.text = ""
+             firstNumber = 0
+            numberFromScreen = 0
+            operation = 0
+        }
+        
         }
 }
 
